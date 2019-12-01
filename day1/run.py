0ab337f1eb594a984599efa30ce7c862e43e5412
Chandler Butler <Chandlerbutler0@gmail.com>
2019-12-01 08:35:12 -0500

    Day1

---
+++ day1/run.py
@@ -0,0 +1,26 @@
+def runProblem1():
+    runningTotal = 0
+    with open('adventOfCode2019\\day1\\input.txt') as data:
+        for line in data.readlines():
+            mass = int(line.rstrip())
+            fuel = (mass//3 - 2)
+            runningTotal += fuel
+        print(runningTotal)
+
+
+def runProblem2():
+    runningTotal = 0
+    with open('adventOfCode2019\\day1\\input.txt') as data:
+        for line in data.readlines():
+            mass = int(line.rstrip())
+            fuel = determineFuel(mass)
+            runningTotal += fuel
+        print(runningTotal)
+
+
+## 1969 requires 966
+def determineFuel(mass):
+    if (mass//3 - 2) < 0:
+        return 0
+    else:
+        return (mass//3 - 2) + determineFuel((mass//3 - 2))
---
+++ test.py
@@ -0,0 +1,4 @@
+from day1 import run as day1
+
+day1.runProblem1()
+day1.runProblem2()
